f7faf77d75157873813077d1f442151e68b6384b
Leni <leni.mette@outlook.fr>
2025-12-12 18:37:21 +0100

    dzdzd

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 import planets from "./data/planets.js";
 import drawSun from "./render/canvas.js";
+import iniControls from "./ui/controls.js";
 
 
 
 // Dessine soleil
 drawSun();
 
+//Event au clique
+iniControls();
+
+
+//Test :
 // console.log(planets);
--- js/ui/controls.js
+++ js/ui/controls.js
@@ -1,3 +1,18 @@
-const button = document.querySelectorAll(".blocLeft button");
+const buttons = document.querySelectorAll(".blocLeft button");
 
-button.addEventListener()
+// Identifié la planet cliqué
+function iniControls(planet) {
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            let data = button.dataset.planet
+            console.log(data);
+        })
+    })
+}
+
+//Changer couleur canvas
+
+
+//displayInformations
+
+export default iniControls;
