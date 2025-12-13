cb456e8a017182002dc6853892102d22b9ccfb54
Leni <leni.mette@outlook.fr>
2025-12-13 18:08:16 +0100

    END§§§§§

--- js/render/informations.js
+++ js/render/informations.js
@@ -4,7 +4,7 @@ const infoContainer = document.getElementById("info-container");
 async function displayInformations(planetName) {
   const planet = planets.find((p) => p.name === planetName);
   if (planet) {
-    // ⚠️ ATTEND la promesse
+
     const moons = await planet.moons;
     console.log(moons);
     const firstMoons = moons.slice(0, 10); // Affiche jusqu'à 10 lunes
@@ -12,7 +12,7 @@ async function displayInformations(planetName) {
 
     infoContainer.innerHTML = `
       <h2>${planet.name}</h2>
-      <img src="../img/${planet.name}.png" alt="${planet.name}" />
+      <img src="img/${planet.name}.png" alt="${planet.name}" />
       <p class="title">Lune:</p><p class="value">${moonsText}</p><br>
       <p class="title">Rayon:</p><p class="value">${planet.size}.000 km</p><br>
       <p class="title">Distance du soleil:</p><p class="value">${planet.distance}.000.000 km</p><br>
