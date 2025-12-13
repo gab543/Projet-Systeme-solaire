e199d2418ad6f597477eb56c4efe94d991de5bf0
Gabriel <gabcab1002@gmail.com>
2025-12-13 17:22:51 +0100

    refactor: Simplify button element and update moon data handling in API

--- js/app.js
+++ js/app.js
@@ -3,10 +3,7 @@ import { animate } from "./render/canvas.js";
 import iniButtons from "./ui/buttons.js";
 
 console.log(planets);
-//Event au clique
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("btnDefault").click();
-});
+
 animate();
 iniButtons();
 //Test :
